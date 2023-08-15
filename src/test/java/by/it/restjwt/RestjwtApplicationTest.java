003c817501ab27e37d5d818acd31943c719af8ec
Nikolay1992167 <wulik1081@mail.ru>
2023-08-15 22:58:37 +0300

    Project starts, add liquibase.

---
+++ src/test/java/by/it/restjwt/RestjwtApplicationTest.java
@@ -0,0 +1,13 @@
+package by.it.restjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestjwtApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
